39c39da193fd33d04f28ac01600a0329e771b1b0
OskarPrzydatek <o.przydatek@wp.pl>
2021-03-02 13:47:07 +0100

    add global style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import "./App.scss";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import "./style/style.scss";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Currencies from "./pages/currencies/Currencies";
 import Gold from "./pages/gold/Gold";
 
 export default function App() {
   return (
-    <div className="App">
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
@@ -15,6 +14,5 @@ export default function App() {
           <Route path='/gold' component={Gold} />
         </Switch>
       </BrowserRouter>
-    </div>
   );
 }
