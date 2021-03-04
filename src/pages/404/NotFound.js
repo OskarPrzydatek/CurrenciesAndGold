cb0f23e81c422851509ea687f0e316c777d9ff44
OskarPrzydatek <o.przydatek@wp.pl>
2021-03-04 11:42:30 +0100

    change links on 404 page

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -6,10 +6,10 @@ export default function NotFound() {
     return (
         <section className="not-found">
             <article className="not-found__currencies not-found__currencies-animation">
-                <Link to='/'>Brak walut!</Link>
+                <Link to='/currencies'>Brak walut!</Link>
             </article>
             <article className="not-found__gold not-found__gold-animation">
-                <Link to='/'>Brak złota!</Link>
+                <Link to='/gold'>Brak złota!</Link>
             </article>
         </section>
     )
